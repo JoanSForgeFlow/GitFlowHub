38b71fdd98815e0db4a796328e6285fdbf1f3f49
Alejandro Alarcon <alejandro.alarcon.cano@gmail.com>
2023-07-11 21:32:52 +0200

    [ADD] First endpoint created - Register user

---
+++ Backend/Controllers/UserController.ts
@@ -0,0 +1,10 @@
+import prisma from "../Middlewares/prisma-client";
+
+const RegisterUser = async (req, res) => {
+    const data = req.body;
+    const createUser = await prisma.user.create({ data });
+    res.status(200).json(createUser);
+  };
+
+  export {RegisterUser};
+
--- Backend/Routes/UserRoutes.ts
+++ Backend/Routes/UserRoutes.ts
@@ -2,10 +2,9 @@ import express from "express";
 
 const router = express.Router();
 
-import { RegisterUser, PerfilUser } from "../Controllers/UserController.js";
+import { RegisterUser } from "../Controllers/UserController.js";
 
 //Tendremos las rutas de login y de display del perfil del user
 router.post("/login", RegisterUser);
-router.get("/profile/:id", PerfilUser);
 
 export default router;
