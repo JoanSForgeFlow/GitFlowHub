b5a9af2984612e707c74c20513beff6c3df0e6f6
Joan Sisquella <joan.sisquella@forgeflow.com>
2023-07-13 16:59:30 +0200

    [IMP] show PR cards horizontally if possible

--- gitflowhub-react-app/src/App.tsx
+++ gitflowhub-react-app/src/App.tsx
@@ -111,30 +111,32 @@ const App: React.FC = () => {
           pull.user.login.toLowerCase().includes(searchUser.toLowerCase()) &&
           pull.title.toLowerCase().includes(searchTitle.toLowerCase())
         )
-      ).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' })).map((repoName) => {
-        return (
-          <div key={repoName} className="repo-group">
-            <h2 onClick={() => handleRepoClick(repoName)}>{repoName}</h2>
-            {(autoExpand || expandedRepos.has(repoName)) && repoGroups[repoName].filter(pull => 
-              pull.user.login.toLowerCase().includes(searchUser.toLowerCase()) &&
-              pull.title.toLowerCase().includes(searchTitle.toLowerCase())
-            ).map((pull: Pull) => (
-              <div key={pull.id} className="card">
-                <div className="card-header">
-                  <img src={pull.user.avatar_url} alt="User avatar" className="user-info__img" />
-                  <h3>{pull.title}</h3>
-                  <GoIcon url={pull.html_url} />
+      ).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' })).map((repoName) => (
+        <div key={repoName} className="repo-group">
+          <h2 onClick={() => handleRepoClick(repoName)}>{repoName}</h2>
+          {(autoExpand || expandedRepos.has(repoName)) && (
+            <div className="pull-cards"> 
+              {repoGroups[repoName].filter(pull => 
+                pull.user.login.toLowerCase().includes(searchUser.toLowerCase()) &&
+                pull.title.toLowerCase().includes(searchTitle.toLowerCase())
+              ).map((pull: Pull) => (
+                <div key={pull.id} className="card">
+                  <div className="card-header">
+                    <img src={pull.user.avatar_url} alt="User avatar" className="user-info__img" />
+                    <h3>{pull.title}</h3>
+                    <GoIcon url={pull.html_url} />
+                  </div>
+                  <div className="card-body">
+                    <p>Submitted by: {pull.user.login}</p>
+                    <p>State: {pull.state}</p>
+                    <p>Created at: {pull.created_at}</p>
+                  </div>
                 </div>
-                <div className="card-body">
-                  <p>Submitted by: {pull.user.login}</p>
-                  <p>State: {pull.state}</p>
-                  <p>Created at: {pull.created_at}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        );
-      })}
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 };
