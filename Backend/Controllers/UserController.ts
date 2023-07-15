bd927f90b421b309d046cf674a58d84576d6e6f7
Alejandro Alarcon <alejandro.alarcon.cano@gmail.com>
2023-07-15 12:21:27 +0200

    [ADD] User Controller and user routes developed

--- Backend/Controllers/UserController.ts
+++ Backend/Controllers/UserController.ts
@@ -1,5 +1,7 @@
 import prisma from "../Middlewares/prisma-client.js";
 import bcrypt from "bcryptjs";
+import generarId from "../Helpers/generarId.js";
+import generarJWT from "../Helpers/generarJWT.js";
 
 const RegisterUser = async (req, res) => {
   const data = req.body;
@@ -10,10 +12,18 @@ const RegisterUser = async (req, res) => {
   data.password = hashedPassword;
 
   const createUser = await prisma.user.create({ data });
+
+  //Generate a token that will be used to confirm account
+
+  const token = generarId();
+
+  const updatedUser = await prisma.user.update({
+    where: { id: createUser.id },
+    data: { token },
+  });
+
   res.status(200).json(createUser);
 };
-
-const authenticateUser = async (req, res) => {};
 
 const confirmUser = async (req, res) => {
   //Extract token from the header
@@ -23,8 +33,12 @@ const confirmUser = async (req, res) => {
   const searchedUser = await prisma.user.findFirstOrThrow({ where: { token } });
 
   if (searchedUser) {
-    searchedUser.confirmed = true;
-    searchedUser.token = "";
+    //If the user exists, account is confirmed and token is removed
+    const updatedUser = await prisma.user.update({
+      where: { id: searchedUser.id },
+      data: { confirmed: true, token: "" },
+    });
+    res.status(200).json({ msg: "User confirmation sucess" });
   }
 };
 
@@ -33,17 +47,31 @@ const LogInUser = async (req, res) => {
   const { email, password } = data;
 
   // Check if the user exists
-  const user = await prisma.user.findUnique({ where: { email } });
+  const searchedUser = await prisma.user.findUniqueOrThrow({
+    where: { email },
+  });
 
-  // If the user doesn't exist, a json will show
-  if (!user) {
+  // If the user doesn't exist or is not confirmed, a json will show
+  if (!searchedUser) {
     const error = new Error("User doesn't exist");
+    return res.status(404).json({ msg: error.message });
+  } else if (searchedUser.confirmed === false) {
+    const error = new Error("Your account needs to be confirmed");
     return res.status(404).json({ msg: error.message });
   }
 
-  const passwordMatch = await bcrypt.compare(password, user.password);
+  //Check the password of the user
+  const passwordMatch = await bcrypt.compare(password, searchedUser.password);
 
   if (passwordMatch) {
+    //Create the JWT, that is going to be used on the auth check inside the application
+    const token = generarJWT(searchedUser.id);
+
+    const updatedUser = await prisma.user.update({
+      where: { id: searchedUser.id },
+      data: { confirmed: true, token: token },
+    });
+
     res.status(200).json({ msg: "Login success" });
   } else {
     const error = new Error("Incorrect Password");
@@ -51,4 +79,71 @@ const LogInUser = async (req, res) => {
   }
 };
 
-export { RegisterUser, LogInUser };
+const forgetRequest = async (req, res) => {
+  const { email } = req.body;
+
+  // Check if the user exists
+  const searchedUser = await prisma.user.findUniqueOrThrow({
+    where: { email },
+  });
+
+  // If the user doesn't exist a json will show
+  if (!searchedUser) {
+    const error = new Error("User doesn't exist");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  //if user exists an email will be sent, token is reset
+  const newToken = generarId();
+  const updatedUser = await prisma.user.update({
+    where: { id: searchedUser.id },
+    data: { token: newToken },
+  });
+
+  res.json({
+    msg: "An email to your account has been sent to reset your password",
+  });
+};
+
+const checkToken = async (req, res) => {
+  const { token } = req.params;
+
+  // Check if the token exists
+  const checkedToken = await prisma.user.findFirstOrThrow({ where: { token } });
+
+  if (!checkToken) {
+    const error = new Error("Unvalid Token");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  res.status(200).json({ msg: "Valid Token" });
+};
+
+const newPassword = async (req, res) => {
+  const { token } = req.params;
+  const { password } = req.body;
+
+  // Check if the token exists
+  const searchedUser = await prisma.user.findFirstOrThrow({ where: { token } });
+
+  if (!searchedUser) {
+    const error = new Error("Unvalid Token");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  const updatedUser = await prisma.user.update({
+    where: { id: searchedUser.id },
+    data: { password: password },
+  });
+
+  res.status(200).json({ msg: "Password successfully changed" });
+};
+
+export {
+  RegisterUser,
+  confirmUser,
+  LogInUser,
+  forgetRequest,
+  checkToken,
+  newPassword,
+};
--- Backend/Routes/UserRoutes.ts
+++ Backend/Routes/UserRoutes.ts
@@ -3,13 +3,23 @@ import { errorChecked } from "../Middlewares/controllersMw.js";
 
 const router = express.Router();
 
-import { RegisterUser,LogInUser} from "../Controllers/UserController.js";
+import {
+  RegisterUser,
+  confirmUser,
+  LogInUser,
+  forgetRequest,
+  checkToken,
+  newPassword,
+} from "../Controllers/UserController.js";
 
 //Tendremos las rutas de login y de display del perfil del user
 router.post("/sign-in", errorChecked(RegisterUser));
 router.post("/login", errorChecked(LogInUser));
-router.post("/confirm-account/:token", errorChecked(LogInUser));
-router.post("/forget-password", errorChecked(LogInUser));
-router.post("/forget-password/:token", errorChecked(LogInUser));
+router.get("/confirm-account/:token", errorChecked(confirmUser));
+router.post("/forget-password", errorChecked(forgetRequest));
+router
+  .route("/forget-password/:token")
+  .get(errorChecked(checkToken))
+  .post(errorChecked(newPassword));
 
 export default router;
