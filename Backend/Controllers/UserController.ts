300a8e887ee9183e92774b2f564585757f29c518
Alejandro Alarcon <alejandro.alarcon.cano@gmail.com>
2023-07-15 11:13:55 +0200

    [ADD] Define auth routes

--- Backend/Controllers/UserController.ts
+++ Backend/Controllers/UserController.ts
@@ -1,44 +1,54 @@
 import prisma from "../Middlewares/prisma-client.js";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 
 const RegisterUser = async (req, res) => {
-    const data = req.body;
+  const data = req.body;
 
-    const {password}=data
-    const hashedPassword = await bcrypt.hash(password, 10)
+  const { password } = data;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    data.password=hashedPassword
+  data.password = hashedPassword;
 
-    const createUser = await prisma.user.create({ data });
-    res.status(200).json(createUser);
-  };
+  const createUser = await prisma.user.create({ data });
+  res.status(200).json(createUser);
+};
 
-  const LogInUser=async(req,res)=>{
-    const data= req.body;
-    const {email,password}=data
+const authenticateUser = async (req, res) => {};
 
-    // Check if the user exists
-    const user= await prisma.user.findUnique({where:{email}})
+const confirmUser = async (req, res) => {
+  //Extract token from the header
+  const { token } = req.params;
 
-    // If the user doesn't exist, a json will show
-    if (!user){
-      const error = new Error("User doesn't exist");
-      return res.status(404).json({ msg: error.message });
+  //Search on db for this token
+  const searchedUser = await prisma.user.findFirstOrThrow({ where: { token } });
 
-    }
+  if (searchedUser) {
+    searchedUser.confirmed = true;
+    searchedUser.token = "";
+  }
+};
 
-    
-    const passwordMatch = await bcrypt.compare(password, user.password);
+const LogInUser = async (req, res) => {
+  const data = req.body;
+  const { email, password } = data;
 
-    if (passwordMatch){
-        res.status(200).json({msg: "Login success"})
-    } else{
-        const error = new Error("Incorrect Password");
-        return res.status(404).json({ msg: error.message });
-    }
+  // Check if the user exists
+  const user = await prisma.user.findUnique({ where: { email } });
 
-
+  // If the user doesn't exist, a json will show
+  if (!user) {
+    const error = new Error("User doesn't exist");
+    return res.status(404).json({ msg: error.message });
   }
 
-  export {RegisterUser,LogInUser};
+  const passwordMatch = await bcrypt.compare(password, user.password);
 
+  if (passwordMatch) {
+    res.status(200).json({ msg: "Login success" });
+  } else {
+    const error = new Error("Incorrect Password");
+    return res.status(404).json({ msg: error.message });
+  }
+};
+
+export { RegisterUser, LogInUser };
--- Backend/Middlewares/controllersMw.ts
+++ Backend/Middlewares/controllersMw.ts
@@ -1,21 +1,19 @@
-
 const errorHandlerMiddleware = (err, req, res, next) => {
-    res.status(500).json({
-      type: err.constructor.name,
-      message: err.toString(),
-    });
-  };
+  res.status(500).json({
+    type: err.constructor.name,
+    message: err.toString(),
+  });
+};
 
 //Every function from the controllers will pass through the errorChecked MW, who will try catch the request and will report the error if the request fails
-  const errorChecked = (handler) => {
-    return async (req, res, next) => {
-      try {
-        await handler(req, res, next);
-      } catch (err) {
-        next(err);
-      }
-    };
+const errorChecked = (handler) => {
+  return async (req, res, next) => {
+    try {
+      await handler(req, res, next);
+    } catch (err) {
+      next(err);
+    }
   };
-  
-  export { errorHandlerMiddleware, errorChecked };
-  
+};
+
+export { errorHandlerMiddleware, errorChecked };
