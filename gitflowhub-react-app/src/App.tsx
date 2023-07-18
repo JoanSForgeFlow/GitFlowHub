3cf81271d40ac5d66dcbded684ef7f38053a5caf
Alejandro Alarcon <alejandro.alarcon.cano@gmail.com>
2023-07-18 21:03:26 +0200

    [ADD] Routing

--- gitflowhub-react-app/src/App.tsx
+++ gitflowhub-react-app/src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
 import PRDashboard from './pages/PRDashboard';
 
+import AuthLayout from './layouts/AuthLayout';
+import LoginUser from './pages/LoginUser';
+import SignInUser from './pages/SignInUser';
+import ForgetPassword from './pages/ForgetPassword';
+import NewPassword from './pages/NewPassword';
+import ConfirmAccount from './pages/ConfirmAccount';
+
 const App: React.FC = () => {
   return (
-    <div>
-      <PRDashboard />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout/>}>
+          <Route index element={<LoginUser/>}/>
+          <Route path='sign-in' element={<SignInUser/>}/>
+          <Route path='forget-password' element={<ForgetPassword/>}/>
+          <Route path='forget-password/:token' element={<NewPassword/>}/>
+          <Route path='confirm-account/:id' element={<ConfirmAccount/>}/>
+          <Route path='main-page' element={<PRDashboard />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
   );
 };
 
