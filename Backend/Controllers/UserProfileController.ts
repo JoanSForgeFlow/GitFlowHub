99d9ca7f94c99172bb7e35a87f5e214d42c7573d
Joan Sisquella <joan.sisquella@forgeflow.com>
2023-08-02 17:34:15 +0200

    [ADD] UserProfile Backend and FrontEnd Form

---
+++ Backend/Controllers/UserProfileController.ts
@@ -0,0 +1,75 @@
+import prisma from "../Middlewares/prisma-client.js";
+
+const getUser = async (req, res) => {
+  const { github_user } = req.params;
+
+  const user = await prisma.user.findUnique({
+    where: { github_user: github_user },
+    select: {
+      id: true,
+      email: true,
+      username: true,
+      token: true,
+      confirmed: true,
+      location: true,
+      language: true,
+      timeZone: true,
+      image: true,
+      github_user: true,
+      login: true,
+      avatar_url: true,
+      company_id: true,
+      Company: true,
+    }
+  });
+
+  if (!user) {
+    return res.status(404).send("User not found");
+  }
+
+  res.json(user);
+};
+
+const getAllCompanies = async (req, res) => {
+  const companies = await prisma.company.findMany();
+
+  res.json(companies);
+};
+
+const updateUser = async (req, res) => {
+  const { github_user } = req.params;
+  const { company_id } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: { github_user: github_user }
+  });
+
+  if (!user) {
+    return res.status(404).send("User not found");
+  }
+
+  const updatedUser = await prisma.user.update({
+    where: { github_user: github_user },
+    data: { company_id },
+    select: {
+      id: true,
+      email: true,
+      username: true,
+      token: true,
+      confirmed: true,
+      location: true,
+      language: true,
+      timeZone: true,
+      image: true,
+      github_user: true,
+      login: true,
+      avatar_url: true,
+      company_id: true,
+      Company: true,
+    }
+  });
+
+  res.json(updatedUser);
+};
+
+export { getUser, getAllCompanies, updateUser };
--- gitflowhub-react-app/src/pages/UserProfile.tsx
+++ gitflowhub-react-app/src/pages/UserProfile.tsx
@@ -1,11 +1,108 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import AuthContext from '../context/AuthProvider';
+import axiosClient from '../config/axiosClient';
+
+interface Company {
+  id: number;
+  name: string;
+}
 
 const UserProfile: React.FC = () => {
+  const { auth, setAuth } = useContext(AuthContext);
+  const [companies, setCompanies] = useState<Company[]>([]);
+  const [selectedCompanyId, setSelectedCompanyId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${auth.token}`,
+        },
+      };
+      
+      try {
+        const response = await axiosClient.get('/companies', config);
+        console.log(`Fetch Companies Response: ${JSON.stringify(response.data)}`);
+        setCompanies(response.data);
+      } catch (error) {
+        console.error(`Error fetching companies: ${error.message}`);
+        if (error.response) {
+          console.error(`Response status: ${error.response.status}`);
+          console.error(`Response data: ${JSON.stringify(error.response.data)}`);
+        }
+      }
+    };
+
+    fetchCompanies();
+  }, []);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(`handleChange called with name: ${event.target.name}, value: ${event.target.value}`);
+    setAuth({
+      ...auth,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleCompanyChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(`handleCompanyChange called with value: ${event.target.value}`);
+    setSelectedCompanyId(Number(event.target.value));
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(`handleSubmit called with github_user: ${auth.github_user}, selectedCompanyId: ${selectedCompanyId}`);
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${auth.token}`,
+      },
+    };
+
+    try {
+      const response = await axiosClient.put(`/${auth.github_user}`, { company_id: selectedCompanyId }, config);
+      console.log(`Update Profile Response: ${JSON.stringify(response.data)}`);
+      setAuth(response.data);
+    } catch (error) {
+      console.error(`Error updating profile: ${error.message}`);
+      if (error.response) {
+        console.error(`Response status: ${error.response.status}`);
+        console.error(`Response data: ${JSON.stringify(error.response.data)}`);
+      }
+    }
+  };
+
   return (
     <div>
-      <h1>This is the user profile page</h1>
+      <h1>User Profile</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" name="username" value={auth.username} onChange={handleChange} />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" value={auth.email} onChange={handleChange} />
+        </label>
+        <label>
+          Github User:
+          <input type="text" name="github_user" value={auth.github_user} onChange={handleChange} />
+        </label>
+        <label>
+          Company:
+          <select value={selectedCompanyId ?? ''} onChange={handleCompanyChange}>
+            <option value="">Select company</option>
+            {companies.map(company => (
+              <option key={company.id} value={company.id}>{company.name}</option>
+            ))}
+          </select>
+        </label>
+        <button type="submit">Update Profile</button>
+      </form>
     </div>
-  )
+  );
 };
 
 export default UserProfile;
