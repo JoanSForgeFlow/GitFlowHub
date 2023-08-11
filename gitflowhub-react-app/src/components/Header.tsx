3cdef8e78816b6812d992ee4c8a1d99351e30537
Alejandro Alarcon <alejandro.alarcon.cano@gmail.com>
2023-08-11 17:04:07 +0200

    [ADD] Sidebar css

--- gitflowhub-react-app/src/components/Header.tsx
+++ gitflowhub-react-app/src/components/Header.tsx
@@ -1,23 +1,83 @@
 import { useNavigate } from "react-router-dom";
+import Drawer from "@mui/material/Drawer";
+import { useState } from "react";
+
+import "../css/LayoutProtectedRoute.css";
+
 
 const Header = ({ username, avatar_url }) => {
   const navigate = useNavigate();
+  const [state, setState] = useState({
+    left: false,
+  });
 
   const handleClick = () => {
-    navigate("/my-profile");
+    navigate("/main-page");
   };
+
+  type Anchor = "left";
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
   return (
-    <div className="flex flex-row items-center bg-gray-900">
+    <div className="flex flex-row items-center bg-gray-900 py-1 border-b border-gray-400">
       <div className="w-10 h-10 mx-2 ">
         <img
           src={avatar_url}
           alt={`${username}'s avatar`}
-          className="rounded-full border-2 border-cyan-950 hover:border-white hover:transition-all hover:duration-500"
+          className="rounded-full border-2 border-cyan-950 hover:border-white hover:transition-all hover:duration-500 cursor-pointer"
+          onClick={toggleDrawer("left", true)}
         />
+        <Drawer
+          anchor={"left"}
+          open={state["left"]}
+          onClose={toggleDrawer("left", false)}
+          PaperProps={{
+            sx: {
+                width: 240,
+                backgroundColor: "rgb(17, 24, 39)"
+              }
+          }}
+        
+        >
+          <div className="px-3">
+            <div className="mt-3 mb-3 bg-gray-900 border-b border-gray-400 text-gray-300">YOUR INFO</div>
+
+            <div className="flex items-center p-0.5 text-gray-300 mb-1 border-2 rounded-lg border-cyan-950 hover:border-white hover:transition-all hover:duration-500 cursor-pointer">
+              <span className="material-symbols-outlined mr-2">home_app_logo</span>
+              Home
+            </div>
+
+            <div className="flex items-center p-0.5 text-gray-300 mb-1 border-2 rounded-lg border-cyan-950 hover:border-white hover:transition-all hover:duration-500 cursor-pointer">
+              <span className="material-symbols-outlined mr-2">person</span>
+              Your Profile
+            </div>
+
+            <div className="flex items-center p-0.5 text-gray-300 mb-1 border-2 rounded-lg border-cyan-950 hover:border-white hover:transition-all hover:duration-500 cursor-pointer">
+              <span className="material-symbols-outlined mr-2">dashboard</span>Your
+              Board
+            </div>
+          </div>
+        </Drawer>
       </div>
 
-      <div className="bg-gray-900 text-white p-1 rounded-lg  border-cyan-950 border-2 hover:border-white hover:transition-all hover:duration-500 ">
-        Main Page
+      <div
+        className="bg-gray-900 text-white px-1 rounded-lg  border-cyan-950 border-2 hover:border-white hover:transition-all hover:duration-500 cursor-pointer home-button "
+        onClick={handleClick}
+      >
+        <span className="material-symbols-outlined ">home_app_logo</span>
       </div>
     </div>
   );
