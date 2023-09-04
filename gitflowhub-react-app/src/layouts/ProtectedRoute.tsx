9c202404c8519d01119312a69ff64003735f529a
Alejandro Alarcon <alejandro.alarcon.cano@gmail.com>
2023-09-04 10:59:51 +0200

    [FIX] Image url when initialize

--- gitflowhub-react-app/src/layouts/ProtectedRoute.tsx
+++ gitflowhub-react-app/src/layouts/ProtectedRoute.tsx
@@ -2,14 +2,18 @@ import { Outlet, Navigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import Header from "../components/Header";
 import { ToastContainer } from "react-toastify";
+import {useEffect} from 'react'
 
 const ProtectedRoute = () => {
-  const { auth, loading } = useAuth();
-
+  const { auth, loading, setAuth } = useAuth();
   const { username, avatar_url } = auth;
 
+  useEffect(() => {
+
+    setAuth(auth)
+  }, [auth])
+  
   console.log(avatar_url);
-  //TODO: añadir un spinner de carga
 
   return (
     <>
