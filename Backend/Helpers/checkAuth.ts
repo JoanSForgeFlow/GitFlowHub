7b2326e087707901b9769c8f2d10a60b15837038
Joan Sisquella <joan.sisquella@forgeflow.com>
2023-09-12 21:32:27 +0200

    [ADD] logs in checkAuth

--- Backend/Helpers/checkAuth.ts
+++ Backend/Helpers/checkAuth.ts
@@ -2,53 +2,59 @@ import jwt from "jsonwebtoken";
 import prisma from "../Middlewares/prisma-client.js";
 
 const checkAuth = async (req, res, next) => {
-  //Auth will be done by using JsonWebTokenError method
+  console.log("Entrando al middleware checkAuth");
 
-  // Bearer es una convencion que se utiliza para enviar el token
-  let token;
+  // Imprimir todos los encabezados de la petición entrante
+  console.log("Encabezados recibidos:", req.headers);
+  console.log(`Accediendo a la ruta: ${req.path}`);
+
+
+  // Verificar si hay una cabecera de autorización
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
+    console.log("Cabecera de autorización encontrada", req.headers.authorization);
+
+    let token;
     try {
-      //req.headers.authorization returns a token Bearer
-      // we want to obtain what it is next to the Bearer, then, we will uncrypt using jwt
       token = req.headers.authorization.split(" ")[1];
+      console.log("Token extraído:", token);
 
+      // Verificar el token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      console.log("Token decodificado con éxito:", decoded);
 
-
+      // Buscar el usuario en la base de datos
       const searchedUser = await prisma.user.findUniqueOrThrow({
         where: { id: parseInt(decoded.id) },
         select: {
           id: true,
           username: true,
           email: true,
-          location:true,
-          language:true,
-          timeZone:true,
-          image:true,
+          location: true,
+          language: true,
+          timeZone: true,
+          image: true,
           github_user: true,
-          avatar_url:true,
-          
+          avatar_url: true,
         },
       });
+      console.log("Usuario encontrado en la base de datos:", searchedUser);
 
-      // Create user variable, we will introduce all data except the password
+      // Añadir el usuario al objeto req
       req.user = searchedUser;
-      //Once we have created the user we will go through the next middleware
+
+      // Ir al siguiente middleware
       return next();
     } catch (error) {
-      return res.status(404).json({ msg: "There is an error" });
+      console.log("Error durante la verificación de autorización:", error);
+      return res.status(404).json({ msg: "Error durante la verificación de autorización" });
     }
   }
 
-  // if the user doesn't send a token, an error will be displayed
-  if (!token) {
-    const error = new Error("Unvalid or expired token");
-    return res.status(401).json({ msg: error.message });
-  }
-  next();
+  console.log("No se encontró la cabecera de autorización o no empieza con Bearer");
+  return res.status(401).json({ msg: "Token no válido o expirado" });
 };
 
 export default checkAuth;
