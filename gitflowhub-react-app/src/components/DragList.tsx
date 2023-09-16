50b59958901328f1e7bb053b9ba942d960a1ccd7
Alejandro Alarcon <alejandro.alarcon.cano@gmail.com>
2023-09-16 10:59:23 +0200

    [FIX] Solve lapiz interface

--- gitflowhub-react-app/src/components/DragList.tsx
+++ gitflowhub-react-app/src/components/DragList.tsx
@@ -11,41 +11,33 @@ interface Option {
   value: string;
 }
 
-const DragList = ({id_PR}) => {
-
-  //TODO: asignee has to be checked in db
-
+const DragList = ({ id_PR }) => {
   const [userOptions, setUserOptions] = useState<Option[]>([]);
   const [isMouseOver, setIsMouseOver] = useState(false);
   const [displayEdit, setDisplayEdit] = useState(false);
 
   const [asignee, setAsignee] = React.useState<string>("");
   const [inputValue, setInputValue] = React.useState("");
-  const { optionUsers,assignUser,getPR,changePRStatus} = useAuth();
+  const { optionUsers, assignUser, getPR, changePRStatus } = useAuth();
 
   useEffect(() => {
     const usersList = async () => {
       const list = await optionUsers();
       setUserOptions(list);
-      
     };
 
-    const loadAsignee= async()=>{
-      const PR = await getPR(id_PR)
-      const loadedAsignee=PR.asigned_user?.username
+    const loadAsignee = async () => {
+      const PR = await getPR(id_PR);
+      const loadedAsignee = PR.asigned_user?.username;
 
       if (loadAsignee) {
-        setAsignee(loadedAsignee)
-        
-      } else{
-        setAsignee("")
-
+        setAsignee(loadedAsignee);
+      } else {
+        setAsignee("");
       }
-
-    }
+    };
     usersList();
-    loadAsignee()
-
+    loadAsignee();
   }, []);
 
   //Function that sets the value to false when leving the asignee div
@@ -58,23 +50,25 @@ const DragList = ({id_PR}) => {
     const assignAction = async () => {
       await assignUser({
         username: asignee || null,
-        id_PR: id_PR
+        id_PR: id_PR,
       });
-  
+
       await changePRStatus({ id: id_PR, status: "Not Started" });
-  
     };
     assignAction();
   };
 
   return (
     <div className="lg:flex lg:w-3/4 md:w-2/3 w-3/4 ">
-      <div onClick={handleMouse} className="w-2rem mr-3 mt-4 font-bold cursor-pointer">
+      <div
+        onClick={handleMouse}
+        className="w-2rem mr-3 mt-4 font-bold cursor-pointer"
+      >
         Assignee:
       </div>
 
       {displayEdit ? (
-        <div className="flex flex-grow justify-center items-center">
+        <div className="flex flex-grow justify-center items-center cursor-pointer">
           <Autocomplete
             value={asignee}
             onChange={(event, newValue) => {
@@ -100,7 +94,7 @@ const DragList = ({id_PR}) => {
           </button>
         </div>
       ) : (
-        <div className="flex w-full justify-center items-center mt-4">
+        <div className=" flex w-full justify-center items-center mt-4 cursor-pointer">
           <div
             className={`w-full ${
               isMouseOver
@@ -116,7 +110,12 @@ const DragList = ({id_PR}) => {
           </div>
 
           {isMouseOver && (
-            <div className="material-symbols-outlined  bg-slate-500 pt-1 rounded-tr-md rounded-br-md">
+            <div
+              onClick={handleMouse}
+              onMouseEnter={() => setIsMouseOver(true)}
+              onMouseLeave={() => setIsMouseOver(false)}
+              className=" material-symbols-outlined  bg-slate-500 pt-1 rounded-tr-md rounded-br-md"
+            >
               Edit
             </div>
           )}
--- gitflowhub-react-app/src/components/StarRating.tsx
+++ gitflowhub-react-app/src/components/StarRating.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 enum Priority {
   LOW = "LOW",
